f562efd72a0e5e8378d8c601bc66e3fb3ded9b59
Adrian Lopez <moarnoizemusic@gmail.com>
2019-05-06 14:57:39 +0200

    CCliente creado y errores de css arreglados

--- src/controller/CAdmin.java
+++ src/controller/CAdmin.java
@@ -1,4 +1,4 @@
-package controler;
+package controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CIndex
  */
-@WebServlet("/CIndex")
-public class cAdmin extends HttpServlet {
+@WebServlet("/CAdmin")
+public class CAdmin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public cAdmin() {
+    public CAdmin() {
         super();
         // TODO Auto-generated constructor stub
     }
