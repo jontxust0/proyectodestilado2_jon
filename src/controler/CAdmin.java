2fe1dfbc9711cd2ed4d10f087915871ce87f6b46
Ibai Acha <achita145@gmail.com>
2019-05-06 14:48:48 +0200

    Administrador, falta rellenar modales

--- src/controler/CAdmin.java
+++ src/controler/CAdmin.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class CIndex
  */
 @WebServlet("/CIndex")
-public class cAdmin extends HttpServlet {
+public class CAdmin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public cAdmin() {
+    public CAdmin() {
         super();
         // TODO Auto-generated constructor stub
     }
